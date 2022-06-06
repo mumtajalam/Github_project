db7acb07f43873e13fec0c35ba838a0514825251
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-06-06 20:25:33 +0530

    update

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ form.addEventListener('submit',(e)=>
             <a href = "https://www.github.com/${searchOriginal}" class="anchor">
             <img src = "${data.avatar_url}"  class="userprofile"/>
             </a>
+            <h3 class="name">${data.name}</h3>
             <span class="userbio">${data.bio}</span> 
             <div color:"green" class="usercompany">Company : ${data.company}</div>
             <div class="userrepo">Public Repo : ${data.public_repos}</div> 
